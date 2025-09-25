272e20e52810536b4de68652614765b95c250297
Rahul Kumar <rahul.kumar@bito.ai>
2025-09-25 17:51:03 +0530

    added l3 error

--- app/src/main/java/com/example/bito_demo/LoginActivity.java
+++ app/src/main/java/com/example/bito_demo/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.example.bito_demo;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.ref.WeakReference;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,21 +23,45 @@ public class LoginActivity extends AppCompatActivity {
         EditText passwordField = findViewById(R.id.password);
         Button loginButton = findViewById(R.id.login_button);
 
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String inputUsername = usernameField.getText().toString();
-                String inputPassword = passwordField.getText().toString();
+        loginButton.setOnClickListener(v -> {
+            String inputUsername = usernameField.getText().toString();
+            String inputPassword = passwordField.getText().toString();
 
-                if (inputUsername.equals(USERNAME) && inputPassword.equals(PASSWORD)) {
-                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+            if (inputUsername.equals(USERNAME) && inputPassword.equals(PASSWORD)) {
+                Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
 
-                } else {
-                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
 
-                    throw new RuntimeException("Intentional crash for Bito testing one");
-                }
+                new BackgroundTask(LoginActivity.this).execute();
+            } else {
+                Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    // AsyncTask with weak reference (looks safe!)
+    private static class BackgroundTask extends AsyncTask<Void, Void, String> {
+        private final WeakReference<LoginActivity> activityRef;
+
+        BackgroundTask(LoginActivity activity) {
+            this.activityRef = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {}
+
+            return "Background result";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            LoginActivity activity = activityRef.get();
+
+
+            activity.findViewById(android.R.id.content).setTag(result);
+        }
     }
 }
