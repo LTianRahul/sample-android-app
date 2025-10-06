ed68c8cc78b8e49999ce33ffd7ca529988938b49
Rahul Kumar <rahul.kumar@bito.ai>
2025-10-06 20:05:56 +0530

    JIRA-POC

--- app/src/main/java/com/example/bito_demo/sampleDemo.java
+++ app/src/main/java/com/example/bito_demo/sampleDemo.java
@@ -36,7 +36,7 @@ public class sampleDemo extends AppCompatActivity {
                     List<Object> data = new ArrayList<>();
                     data.add("Hello");
                     data.add(123);  // adding Integer
-                    System.out.println("JIRA POC TEstim");
+                    System.out.println("JIRA POC Testing");
 
                     for (Object obj : data) {
                         String s = (String) obj;
